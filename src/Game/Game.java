bcf394d1c0a58c7e1f7a0e7b05e15c8e0cceafdc
X5Games / Neyney10 <neyney10@live.com>
2018-12-04 10:33:34 +0200

    update!

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,14 +1,22 @@
 package Game;
 
 import GameObjects.GameObject;
+import Maps.Map;
 
+import java.awt.Rectangle;
 import java.util.ArrayList;
 
 public class Game {
-    ArrayList<GameObject> objects;
-
+    private ArrayList<GameObject> objects;
+    private Map map;
+    
+    
     public Game(ArrayList<GameObject> objects) {
         setObjects(objects);
+    }
+    
+    public Game(String path) {
+    	
     }
 
     public ArrayList<GameObject> getObjects() {
