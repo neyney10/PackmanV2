f94ded9010b6c4e1293641c994aefd0bdabdca0f
neyney10 <neyney10@live.com>
2019-01-02 15:05:06 +0200

    dij

--- src/Algorithms/DijkstraAlgorithm.java
+++ src/Algorithms/DijkstraAlgorithm.java
@@ -18,23 +18,25 @@ import Path.Path;
 import graph.Graph;
 import graph.Graph_Algo;
 import graph.Node;
+import graph.Node_Info;
 
 public class DijkstraAlgorithm implements RobotPathFindingAlgorithm {
 
 	Graph graph;
+	Game game;
 	////////
 	MyCoords mc = new MyCoords();
-
+	Intersect in;
 
 	public DijkstraAlgorithm(Game game) {
 		graph = new Graph();
+		in = new Intersect(game.getMap());
 		refreshGameStatus(game);
 	}
 
 	public void refreshGameStatus(Game game) {
+		this.game = game;
 		Iterator<GameObject> iter;
-
-		Intersect in = new Intersect(game.getMap());
 
 		// BOXES
 		Box box;
@@ -49,24 +51,9 @@ public class DijkstraAlgorithm implements RobotPathFindingAlgorithm {
 			for(int j = 0 ; j < i ; j++) {
 				Vertex v2 = (Vertex) graph.getNodeByIndex(i);
 				//TEMP TEST:
-				boolean isIntersect = false;
-				iter = game.typeIterator(new Box(0));
-				while(iter.hasNext()) {
-					box = (Box) iter.next();
-					Point vp1 = game.getMap().getLocationOnScreen(v1.point);
-					Point vp2 = game.getMap().getLocationOnScreen(v2.point);
-					Line2D line = new Line2D.Double(vp1,vp2);
-
-					if(in.isLineIntersectsWithBox(line, box)) {
-						isIntersect = true;
-						System.out.println("INTERSECTING");
-						break;
-					}
-				}
-				if(!isIntersect)
+				if(!checkIntersect(v1,v2))
 					graph.addEdge(v1.get_name(), v2.get_name(), mc.distance3d(v1.point, v2.point));
 			}
-
 		}
 
 	}
@@ -74,21 +61,31 @@ public class DijkstraAlgorithm implements RobotPathFindingAlgorithm {
 	
 	@Override
 	public Path calculate(Point3D source, Point3D destination) {
-		Node sourceNode = new Vertex(""+source, source);
-		Node destinationNode = new Vertex(""+destination, source);
+		Vertex sourceNode = new Vertex("0"+source, source);
+		Vertex destinationNode = new Vertex("1"+destination, source);
 		graph.add(sourceNode);
 		graph.add(destinationNode);
 		
+		System.out.println(graph.size());
 		// connect those two points to the graph by edges.
 		for(int i = 0 ; i < graph.size() ; i ++) {
-			String nodeName = graph.getNodeByIndex(i).get_name();
-			//if(nodeName.equals(sourceNode) || nodeName.equals(destinationNode)) //
+			Vertex node = (Vertex) graph.getNodeByIndex(i);
+
 			//source
-			double distance = mc.distance3d(source, ((Vertex) graph.getNodeByIndex(i)).point);
-			graph.addEdge(sourceNode.get_name(), nodeName, distance);
+			double distance;
+			if(!checkIntersect(sourceNode,node) && sourceNode != node) {
+				distance = mc.distance3d(source, ((Vertex) graph.getNodeByIndex(i)).point);
+				graph.addEdge(sourceNode.get_name(), node.get_name(), distance);
+				System.out.println("SOURCE ADD EDGE");
+			}
+			
 			//destination
-			distance = mc.distance3d(destination, ((Vertex) graph.getNodeByIndex(i)).point);
-			graph.addEdge(destinationNode.get_name(),nodeName, distance);
+			if(!checkIntersect(destinationNode,node) && destinationNode != node) {
+				distance = mc.distance3d(destination, ((Vertex) graph.getNodeByIndex(i)).point);
+				graph.addEdge(destinationNode.get_name(),node.get_name(), distance);
+				System.out.println(graph.getNodeByName(destinationNode.get_name()) +" | "+(graph.getNodeByName(node.get_name())));
+				System.out.println("destination ADD EDGE");
+			}
 		}
 		
 		// compute the shortest graph using the algorithm
@@ -97,7 +94,7 @@ public class DijkstraAlgorithm implements RobotPathFindingAlgorithm {
 		return parseNodePath(destinationNode.getPath());
 	}
 	
-	public Path parseNodePath(ArrayList<String> nodePath) {
+	private Path parseNodePath(ArrayList<String> nodePath) {
 		Path path = new Path();
 		Vertex v;
 		for(String nodeName : nodePath) {
@@ -106,6 +103,24 @@ public class DijkstraAlgorithm implements RobotPathFindingAlgorithm {
 		}
 		
 		return path;
+	}
+	
+	private boolean checkIntersect(Vertex v1, Vertex v2) {
+		//TEMP TEST:
+		Box box;
+		Iterator<GameObject> iter = game.typeIterator(new Box(0));
+		while(iter.hasNext()) {
+			box = (Box) iter.next();
+			Point vp1 = game.getMap().getLocationOnScreen(v1.point);
+			Point vp2 = game.getMap().getLocationOnScreen(v2.point);
+			Line2D line = new Line2D.Double(vp1,vp2);
+
+			if(in.isLineIntersectsWithBox(line, box)) {
+				return true;
+			}
+		}
+		
+		return false;
 	}
 }
 
